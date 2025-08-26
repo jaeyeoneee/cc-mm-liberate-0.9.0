d6c476ae978caa1522258b3d7c9854ef4fdf9694
jayeoneee <ljy8733@dgist.ac.kr>
2025-08-26 22:31:40 +0900

    add encode and decode functions

--- ccmm/ccmm.py
+++ ccmm/ccmm.py
@@ -9,13 +9,149 @@ from liberate.fhe.data_struct import data_struct
 class CCMM:
   
   def __init__(self):
-    pass
+    # engine 
+    self.params = presets.params["bronze"]
+    self.engine = fhe.ckks_engine(**self.params)
+    
+    # key
+    self.sk = self.engine.create_secret_key()
+    self.pk = self.engine.create_public_key(self.sk)
+
+    # params
+    self.slot_size = self.engine.ctx.N
+    self.num_levels = self.engine.num_levels
+    self.scale = self.engine.scale
+    self.q = self.engine.ctx.q
   
-  def encode(self):
-    pass
+  def engine_info(self):
+    
+    print()
+    print("--------------engine-info-------------------")
+    print(f"num_levels: {self.num_levels}")
+    print(f"num_slots: {self.slot_size}")
+    print(f"scale: {self.scale}")
+    print(f"q: {self.q}")
+    print("--------------------------------------------")
+    
+  def encode(self, matrix:np.ndarray):
+    """
+    인풋으로 넘파이 행렬을 받아서 계수 인코딩으로 암호화한다. 암호문 리스트를 반환한다.
+    
+    Args:
+        matrix (_type_): (self.slot_size, self.slot_siez) 크기의 넘파이 배열
+    
+    Returns:
+        matrix을 행별로 암호화한 리스트
+    """
+    r, c = matrix.shape
+    n = self.slot_size
+
+    # 2차원 확인
+    if matrix.ndim != 2:
+      raise ValueError(f"matrix must to be 2D, got ndim={matrix.ndim}")
+    
+    # 행렬 크기 확인
+    if (r, c) != (n, n):
+      raise ValueError(
+        f"matrix shape must be ({n}, {n}) to match slot_size, but got {matrix.shape}"
+      )
+      
+    cts = []
+    
+    for i in range(n):
+      row = matrix[i]
+      encoded_row = self.engine.encode(row, coeff=True)
+      encrypted_row = self.engine.encrypt(encoded_row, self.pk)
+      cpu_row = self.engine.cpu(encrypted_row)
+      cts.append(cpu_row)
+
+    return cts
   
-  def decode(self):
-    pass
+  def decode(self, cts):
+    """
+    인풋으로 암호문 리스트를 받아서 넘파이 행렬로 반환해준다.
+    
+    Args:
+      cts: liberate fhe로 암호화된 암호문을 가지는 list
+    Returns:
+      cts를 복호화한 numpy 행렬 배열
+    """
+    
+    n = self.slot_size
+    engine = self.engine  
+    
+    if len(cts) != n:
+      raise ValueError(f"cts length must be {n}, got{len(cts)}")
+    
+    rows = []
+    decoded_row = None
+    for ct in cts:
+      
+      ct = engine.cuda(ct)
+      
+      decrypted_row = engine.decrypt(ct, self.sk)
+      
+      decoded_row = engine.decode(decrypted_row, coeff=True)
+    
+      rows.append(decoded_row)  
+  
+    mat = np.vstack(rows)
+    
+    if mat.shape != (n, n):
+      raise ValueError(f"decoded matrix shape {mat.shape} != ({n}, {n})")
+
+    return mat
+  
+  
+  def rotate_with_cyclic_sign_np(self, vector:np.ndarray, shift:int)->np.ndarray:
+    
+    N = vector.shape[-1]
+    r = shift // N
+    s = shift % N
+    
+    out = np.roll(vector, s)
+    
+    if s > 0:
+      out[:s] *= -1
+    
+    if (r % 2) == 1:
+      out *= -1
+      
+    return out
+    
+  
+  def tweak_np(self, matrix: np.ndarray) -> np.ndarray:
+    """
+    Algorithm 1 TWEAK의 Numpy version.
+    """
+    n, N = matrix.shape
+
+    if n == 1:
+      return matrix.copy()
+    
+    result_blocks = [None] * n
+    result_blocks[0] = matrix[0].copy()
+    
+    for ell in range(int(np.log2(n))):
+      two_ell = 2 ** ell
+      block = n // (2*two_ell)
+      
+      aux_inputs = [matrix[(2*j+1)*block] for j in range(two_ell)]
+      aux = self.tweak_np(np.vstack(aux_inputs))
+      
+      for j in range(two_ell):
+        shift = (N//(2**ell))*j
+        rotated = self.rotate_with_cyclic_sign_np(aux[j], shift)
+        
+        result_blocks[j+two_ell] = result_blocks[j] - rotated
+        result_blocks[j]         = result_blocks[j] + rotated
+        
+    return np.vstack(result_blocks)
+
+  def rotate_with_cyclic_sign(self, ct, shift):
+    """
+    multiply ct(X) polynomial by x^i
+    """
   
   def tweak(self):
     pass
--- test/test_ccmm.py
+++ test/test_ccmm.py
@@ -1,5 +1,89 @@
 from ccmm.ccmm import CCMM
+import logging
+import numpy as np
+
+def test_init():
+  
+  model = CCMM()
+
+def test_engine_info():
+  
+  model = CCMM()
+
+  model.engine_info()
 
 def test_encode():
   
   model = CCMM()
+  
+  a = np.ones((model.slot_size, model.slot_size))
+  
+  model.encode(a)
+  
+def test_decode():
+  
+  model = CCMM()
+  
+  a = np.zeros((model.slot_size, model.slot_size), dtype=np.float64)
+  a[1::2, :] = 1.0
+  
+  cts = model.encode(a)
+  
+  matrix = model.decode(cts)
+  
+  assert np.allclose(a, matrix, rtol = 1e-5)
+
+
+def test_rotate_with_cyclic_sign_np():
+  
+  model = CCMM()
+  
+  vector = np.arange(5)
+  
+  print(vector)
+
+  shift1 = 7
+  shift2 = 12
+  
+  vector1 = model.rotate_with_cyclic_sign_np(vector, shift1)
+  vector2 = model.rotate_with_cyclic_sign_np(vector, shift2)
+  
+  assert np.allclose(vector1, np.array([3, 4, 0, -1, -2]), rtol=1e-5)
+  assert np.allclose(vector2, np.array([-3, -4, 0, 1, 2]), rtol=1e-5)
+
+def test_tweak_np():
+  
+  model = CCMM()
+  
+  matrix = np.arange(16).reshape(4, 4)
+  
+  print()
+  print("----------test-tweak-np--------------")
+  print(f"original matrix: {matrix}")
+  
+  n, N = matrix.shape
+  
+  tweaked = []
+  for j in range(n):
+    tweaked_row = np.zeros((N,))
+    for i in range(n):
+      shift = 2*i*j*(N//n)
+      tweaked_row += model.rotate_with_cyclic_sign_np(matrix[i], shift)
+    tweaked.append(tweaked_row)
+  expected = np.vstack(tweaked)
+  print(f"expected tweak result: {expected}")
+  
+  tweak_result = model.tweak_np(matrix)
+  print(f"tweak result:{tweak_result}")
+  
+  assert np.allclose(expected, tweak_result, rtol=0)
+  
+def test_rotate_with_cyclic_sign():
+  
+  model = CCMM()
+  
+  vector = np.arange(2**15)//10
+  encoded = model.engine.encode(vector, coeff=True)
+  encrypted = model.engine.encrypt(encoded, model.pk)
+  
+  
